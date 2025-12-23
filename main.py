5cc6ebb0f755a70e354588446e3d77b8c686237a
pppptttt <pasha.kort@gmail.com>
2025-12-23 19:57:22 +0300

    topic separator interface and basic implementation

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from topic_segmenter.Runner import main
-import nltk
-nltk.download('punkt_tab')
-nltk.download('averaged_perceptron_tagger_eng')
-nltk.download('universal_tagset')
+# from topic_segmenter.Runner import main
+# import nltk
+# nltk.download('punkt_tab')
+# nltk.download('averaged_perceptron_tagger_eng')
+# nltk.download('universal_tagset')
 
-json_input = "raw_data/telegram_prepared.json"
-main(json_input)
+# json_input = "raw_data/telegram_prepared.json"
+# main(json_input)
+
+from topic_segmentor import TimeGapTopicSegmentor
+
+seg = TimeGapTopicSegmentor(max_gap_seconds=120)
+topics = seg.get_topics("raw_data/messages.json")
+
+print(len(topics))
+for t in topics:
+    for msg in t:
+        print(f"{msg.user}: {msg.text}")
+    print("\n\n")
+# print([len(t) for t in topics[:5]])
---
+++ topic_segmentor/__init__.py
@@ -0,0 +1,4 @@
+from .topic_segmentor import TopicSegmentor, ChatMessage, Topic
+from .time_gap_separator import TimeGapTopicSegmentor
+
+__all__ = ["TopicSegmentor", "ChatMessage", "Topic", "TimeGapTopicSegmentor"]
