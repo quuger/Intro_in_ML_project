dc15390e56e98f1354d21161c846008c8d391384
domosedy <ilayvin05@mail.ru>
2025-12-26 02:01:35 +0300

    init

---
+++ third_party/topic_segmentor/grammar/SentenceGrammarAnalyzer.py
@@ -0,0 +1,34 @@
+import nltk
+
+
+class SentenceGrammarAnalyzer:
+    REPLY_POS_VALID_UNIVERSAL_TAGS = set(["CONJ"])
+    # check http://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html for
+    # a good list
+    REPLY_POS_VALID_UPENN_TAGS = set(["WDT", "DT"])
+    REPLY_STARTERS = set(
+        ["ok", "ok.", "k", "k." "mine", "his", "hers", "theirs", "ours"]
+    )
+
+    def __init__(self, message, tokenizer):
+        self.message = message
+        self.tokenizer = tokenizer
+
+    def isAReply(self):
+        stemmedTokens = self.tokenizer.stemAndTokenize(self.message)
+
+        if len(stemmedTokens) <= 1:
+            return (True, "stemmed length of " + str(len(stemmedTokens)))
+
+        tokens = self.tokenizer.tokenize(self.message)
+        univeralTags = nltk.pos_tag(tokens, tagset="universal")
+
+        if univeralTags[0][1] in SentenceGrammarAnalyzer.REPLY_POS_VALID_UNIVERSAL_TAGS:
+            return (True, "universal tag " + univeralTags[0][1])
+
+        upennTags = nltk.pos_tag(tokens)
+        if upennTags[0][1] in SentenceGrammarAnalyzer.REPLY_POS_VALID_UPENN_TAGS:
+            return (True, "upenn tag " + upennTags[0][1])
+        if tokens[0].lower() in SentenceGrammarAnalyzer.REPLY_STARTERS:
+            return (True, "reply starter " + tokens[0].lower())
+        return (False, "not a reply")
---
+++ third_party/topic_segmentor/model/Model.py
@@ -0,0 +1,49 @@
+import gensim
+
+import numpy as np
+
+
+class Model:
+    def __init__(self, messages, tokenizer):
+        self.tokenizer = tokenizer
+        self.innerModel = self.trainNewModel(messages)
+        self.index2wordSet = set(self.innerModel.wv.index_to_key)
+
+    def trainNewModel(self, messages):
+        return gensim.models.Word2Vec(
+            [self.tokenizer.stemAndTokenize(message) for message in messages],
+            min_count=1,
+        )
+
+    def __getitem__(self, index):
+        return self.innerModel.wv.get_vector(index)
+
+    def calculateSimilarity(self, messageA, messageB, indexDistance):
+        fullTokensA = self.tokenizer.stemAndTokenize(messageA)
+        fullTokensB = self.tokenizer.stemAndTokenize(messageB)
+
+        width = 10
+        startA = 0
+        best = (float("inf"), 0)
+        decay = 0.993**indexDistance
+        while startA < len(fullTokensA):
+            startB = 0
+            tokensA = fullTokensA[startA : (startA + width)]
+            while startB < len(fullTokensB):
+                tokensB = fullTokensB[startB : (startB + width)]
+                cosine = self.innerModel.wv.n_similarity(tokensA, tokensB) * decay
+                centroid = self.centroidDistance(tokensA, tokensB) / decay
+                pair = (centroid, cosine)
+                if best is None or best > pair:
+                    best = pair
+                startB = startB + width // 2
+            startA = startA + width // 2
+        return best
+
+    def centroidDistance(self, tokensA, tokensB):
+        centroidA = sum([self[t] for t in tokensA]) / len(tokensA)
+        centroidB = sum([self[t] for t in tokensB]) / len(tokensB)
+        return np.linalg.norm(self.centroid(tokensA) - self.centroid(tokensB))
+
+    def centroid(self, tokens):
+        return sum([self[t] for t in tokens]) / len(tokens)
