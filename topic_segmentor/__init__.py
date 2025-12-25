6603d2b98df4721809474609e92adbd2c23cdb37
pppptttt <pasha.kort@gmail.com>
2025-12-25 19:04:45 +0300

    limit topics by size

--- topic_segmentor/__init__.py
+++ topic_segmentor/__init__.py
@@ -1,4 +1,4 @@
 from .topic_segmentor import TopicSegmentor, ChatMessage, Topic
-from .time_gap_separator import TimeGapTopicSegmentor
+from .time_gap_segmentor import TimeGapTopicSegmentor
 
 __all__ = ["TopicSegmentor", "ChatMessage", "Topic", "TimeGapTopicSegmentor"]
